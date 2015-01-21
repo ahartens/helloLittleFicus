3b13270a9b9e5418c628060ea5deb27dd1a65f55
ahartens <ahartens@gmail.com>
2015-01-21 09:35:58 +0100

    added basic kmeans algorithm

---
+++ allenmvn/src/main/java/allen2hpo/clustering/AlgBasic.java
@@ -0,0 +1,28 @@
+package allen2hpo.clustering.kmeans;
+
+import allen2hpo.clustering.*;
+import allen2hpo.matrix.Matrix;
+
+public class AlgBasic implements Kmeansable{
+	private KmeansObject kmeans = null;
+	
+	public AlgBasic(Matrix m){
+		kmeans = new KmeansObject(m);
+
+	}
+
+	public void setK(){
+		kmeans.setK(3);
+	}		
+	public void setInitClusters(){
+		kmeans.setInitClustersBasic();
+
+	}					
+	public void setDistCalc(DistanceComputable d){
+		kmeans.setDistCalcBasic();
+
+	}
+	public void beginClustering(int i){
+		kmeans.beginClustering(i);
+	}
+}
