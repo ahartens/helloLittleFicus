ee39cdc64c70ae6d3999a33bbcf42b87f3084e7a
ahartens <ahartens@gmail.com>
2015-04-27 10:40:03 +0200

    adding phenotype to gene

---
+++ allenmvn/src/main/java/allen2hpo/hpo/ReadHPOAnnotsPhenotypeToGene.java
@@ -0,0 +1,113 @@
+package allen2hpo.hpo;
+
+import allen2hpo.allen.parsing.ReadAnnots;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+
+
+
+/**
+*	Reads /Probes.csv file line by line, storing whatever info required in 
+*	arrays
+*	Probe ID number is stored in ids array
+*/
+public class ReadHPOAnnotsPhenotypeToGene extends ReadAnnots{
+
+	//__________________________________________________________________________
+    //
+    //  Variables                             
+    //__________________________________________________________________________
+
+	/**	list of entrez gene symbols */
+	ArrayList<String> entrezGeneSymbols = null;
+
+	/**	list of hpo terms */
+	ArrayList<String> hpoTermNames = null;
+
+	//__________________________________________________________________________
+    //
+    //  Getters                             
+    //__________________________________________________________________________
+
+	/**
+	*
+	*/
+	public ArrayList<String> getEntrezGeneSymbols(){
+		return this.entrezGeneSymbols;
+	}
+
+	/**
+	*
+	*/
+	public ArrayList<String> getHpoTermNames(){
+		return this.hpoTermNames;
+	}
+
+
+
+	//__________________________________________________________________________
+    //
+    //  Constructor                             
+    //__________________________________________________________________________
+
+	public ReadHPOAnnotsPhenotypeToGene(String file){
+		/*
+		*	Set filename to be parsed
+		*/
+		super.setFilename(file);
+
+		/*
+		*	Init arraylists to read data
+		*/
+		this.entrezGeneSymbols = new ArrayList<String>();
+		this.hpoTermNames = new ArrayList<String>();
+
+		/*
+		*	Begin reading file line by line. Line is handled here.
+		*/
+		super.parseFile();
+	}
+
+
+
+	//__________________________________________________________________________
+    //
+    //  Methods                             
+    //__________________________________________________________________________
+
+	@Override 
+	public void handleRow(String line, int ri){
+		
+		/*
+		*	Input is a csv and some cells contains further commas (in 
+		*	tissue name). These cells have quotes delimiting cell.
+		*	Format :
+		*	[0]		   [1]		 [2] [3]         [4]  
+		*	(id,_,_,_)"(acronym)"(,)"(long name)"(_,x,y,z,_,_,_)
+		*	Thus first split line into components separated by commas. 
+		*	Handle results separately
+		*/
+
+		
+		Scanner lineSc = new Scanner(line);
+		lineSc.useDelimiter("\t");
+		int i = 0;
+		while (lineSc.hasNext()) {
+			if(i==1){
+				this.hpoTermNames.add(lineSc.next());
+
+			}
+			else if(i==3){
+				this.entrezGeneSymbols.add(lineSc.next());
+			}
+			else{
+				lineSc.next();
+			}
+			i++;
+		}
+
+			
+    }
+}
